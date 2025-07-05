085f82bc54617f16e904d04b9c2f92f873eb1766
Shivam Shivanshu <shivamshivanshu15@gmail.com>
2025-07-05 16:57:41 +1000

    Treesitter plugin

---
+++ nvim/lua/shivam/plugins/treesitter.lua
@@ -0,0 +1,51 @@
+return {
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	event = { "BufReadPost", "BufNewFile" },
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter-textobjects", -- Optional
+	},
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			-- Install these languages
+			ensure_installed = {
+				"lua", "python", "bash", "c", "cpp",
+				"json", "markdown", "yaml", "vim", 
+			},
+			auto_install = true, -- Automatically install missing parsers
+
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false,
+			},
+
+			indent = {
+				enable = true,
+			},
+
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<CR>",
+					node_incremental = "<CR>",
+					scope_incremental = "<TAB>",
+					node_decremental = "<BS>",
+				},
+			},
+
+			textobjects = {
+				select = {
+					enable = true,
+					lookahead = true,
+					keymaps = {
+						["af"] = "@function.outer",
+						["if"] = "@function.inner",
+						["ac"] = "@class.outer",
+						["ic"] = "@class.inner",
+					},
+				},
+			},
+		})
+	end,
+}
+
