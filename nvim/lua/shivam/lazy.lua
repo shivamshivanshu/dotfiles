37424edf8fe2a51bb2330adb2a7a19609bdf0cf0
Shivam Shivanshu <shivamshivanshu15@gmail.com>
2025-07-05 16:52:17 +1000

    Add Lazy plugin manager

---
+++ nvim/lua/shivam/lazy.lua
@@ -0,0 +1,21 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+-- Load plugins from lua/shivam/plugins/*.lua
+require("lazy").setup("shivam.plugins", {
+	change_detection = {
+		notify = false,
+	},
+})
