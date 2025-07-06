cf28f72e9100d485a6033d89d2904c167c8263a5
Shivam Shivanshu <shivamshivanshu15@gmail.com>
2025-07-07 09:57:58 +1000

    Setup guessindent

--- nvim/lua/shivam/plugins/guessindent.lua
+++ nvim/lua/shivam/plugins/guessindent.lua
@@ -1,3 +1,7 @@
 return {
-	'NMAC427/guess-indent.nvim' -- Detect tabstop and shiftwidth automatically
+	'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
+	config = function()
+		require("guess-indent").setup({})
+	end
+
 }
