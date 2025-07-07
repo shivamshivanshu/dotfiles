5f92453e88a12e47ae9938b632549054ccd87f37
Shivam Shivanshu <shivamshivanshu15@gmail.com>
2025-07-07 10:22:51 +1000

    Add filepath to lualine.nvim

--- nvim/lua/shivam/plugins/lualine.lua
+++ nvim/lua/shivam/plugins/lualine.lua
@@ -5,7 +5,15 @@ return {
 		require("lualine").setup({
 			options = {
 				theme = "gruvbox"
-			}
+			},
+			sections = {
+				lualine_c = {
+					{
+						'filename',
+						path = 1, -- 0 = filename, 1 = relative path, 2 = absolute path
+					},
+				},
+			},
 		})
 	end
 }
