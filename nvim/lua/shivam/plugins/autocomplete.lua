6c64e2314071b5f0eb790a661a88daafbd6279fa
Shivam Shivanshu <shivamshivanshu15@gmail.com>
2025-08-06 14:11:57 +1000

    Create autocomplete.lua

---
+++ nvim/lua/shivam/plugins/autocomplete.lua
@@ -0,0 +1,61 @@
+return {
+	"hrsh7th/nvim-cmp",
+	dependencies = {
+		"hrsh7th/cmp-nvim-lsp", -- LSP source for nvim-cmp
+		"hrsh7th/cmp-buffer", -- buffer completions
+		"hrsh7th/cmp-path", -- filesystem paths
+		"saadparwaiz1/cmp_luasnip", -- snippet completions
+		"L3MON4D3/LuaSnip", -- snippet engine
+		"rafamadriz/friendly-snippets",
+	},
+	config = function()
+		local cmp     = require("cmp")
+		local luasnip = require("luasnip")
+
+		-- Load VSCode-style snippets
+		require("luasnip.loaders.from_vscode").lazy_load()
+
+		cmp.setup({
+			snippet = {
+				expand = function(args)
+					luasnip.lsp_expand(args.body)
+				end,
+			},
+			mapping = cmp.mapping.preset.insert({
+				["<C-Space>"] = cmp.mapping.complete(),
+				["<CR>"]      = cmp.mapping.confirm({ select = true }),
+				["<C-n>"]     = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_next_item()
+					elseif luasnip.expand_or_jumpable() then
+						luasnip.expand_or_jump()
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+				["<C-p>"]   = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_prev_item()
+					elseif luasnip.jumpable(-1) then
+						luasnip.jump(-1)
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+			}),
+			sources = cmp.config.sources({
+				{ name = "nvim_lsp" },
+				{ name = "luasnip" },
+			}, {
+				{ name = "buffer" },
+				{ name = "path" },
+			}),
+		})
+
+		-- Optional: enable completion in the command line
+		cmp.setup.cmdline("/", {
+			mapping = cmp.mapping.preset.cmdline(),
+			sources = { { name = "buffer" } },
+		})
+	end
+}
