e40f101d7ab3e8f3d792c81459d0b07b94646499
Shivam Shivanshu <shivamshivanshu15@gmail.com>
2025-08-19 15:03:55 +1000

    Blankline.lua

---
+++ nvim/lua/shivam/plugins/blankline.lua
@@ -0,0 +1,8 @@
+return {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    opts = {
+        indent = { char = "│" }, -- character for indent guides
+        scope = { enabled = true, show_start = true, show_end = true },
+    },
+}
