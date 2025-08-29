c20f4ca01a6f605b7c1ea190faac83dcc67620ff
Shivam Shivanshu <shivamshivanshu15@gmail.com>
2025-08-29 16:38:08 +1000

    Update keymaps.lua with copy cwd

--- nvim/lua/shivam/keymaps.lua
+++ nvim/lua/shivam/keymaps.lua
@@ -24,3 +24,29 @@ map("n", "<leader>j", "<C-w>j")
 map("n", "<leader>k", "<C-w>k")
 map("n", "<leader>l", "<C-w>l")
 
+-- Copy cwd to clipboard
+vim.keymap.set("n", "<leader>cp", function()
+  local path = nil
+
+  -- Try oil.nvim first
+  local oil_ok, oil = pcall(require, "oil")
+  if oil_ok and oil.get_current_dir and vim.bo.filetype == "oil" then
+    path = oil.get_current_dir()
+  else
+    -- Otherwise use the buffer's file path
+    local buf_path = vim.fn.expand("%:p")
+    if buf_path ~= "" then
+      path = buf_path
+    end
+  end
+
+  if not path then
+    print("No valid path to copy")
+    return
+  end
+
+  -- Copy to system clipboard
+  vim.fn.setreg("+", path)
+  print("Copied: " .. path)
+end, { desc = "Copy file/dir path to clipboard" })
+
