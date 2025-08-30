01e3fc28b88a94495dde9b17977a7f4d2fac4256
Shivam Shivanshu <shivamshivanshu15@gmail.com>
2025-08-31 04:26:22 +1000

    Update wezterm.lua

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -11,8 +11,8 @@ config.color_schemes = {
     cursor_fg     = "#1D2021",
     selection_bg  = "#D4BE98",
     selection_fg  = "#3C3836",
-    ansi          = { "#1d2021","#ea6962","#a9b665","#d8a657","#7daea3","#d3869b","#89b482","#d4be98" },
-    brights       = { "#eddeb5","#ea6962","#a9b665","#d8a657","#7daea3","#d3869b","#89b482","#d4be98" },
+    ansi          = { "#1d2021", "#ea6962", "#a9b665", "#d8a657", "#7daea3", "#d3869b", "#89b482", "#d4be98" },
+    brights       = { "#eddeb5", "#ea6962", "#a9b665", "#d8a657", "#7daea3", "#d3869b", "#89b482", "#d4be98" },
   },
 }
 config.color_scheme = "gruvbox_material_dark_hard"
@@ -22,5 +22,21 @@ config.font = wezterm.font("JetBrains Mono")
 config.font_size = 14.0
 config.hide_tab_bar_if_only_one_tab = true
 config.window_padding = { left = 8, right = 8, top = 6, bottom = 6 }
+config.mouse_bindings = {
+  {
+    event = { Up = { streak = 1, button = "Left" } },
+    mods = "CMD",
+    action = wezterm.action.OpenLinkAtMouseCursor,
+  },
+}
+config.visual_bell = {
+  fade_in_duration_ms = 150,
+  fade_out_duration_ms = 150,
+  target = "CursorColor",
+}
+
+config.keys = {
+  { key = "f", mods = "CTRL|SHIFT", action = wezterm.action.Search("CurrentSelectionOrEmptyString") },
+}
 
 return config
