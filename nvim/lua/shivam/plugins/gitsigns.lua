bc3d881d7e438d7f9a8d814f0890ea5635f3e813
Shivam Shivanshu <shivamshivanshu15@gmail.com>
2025-10-29 10:14:55 +0530

    Update gitsigns.lua

--- nvim/lua/shivam/plugins/gitsigns.lua
+++ nvim/lua/shivam/plugins/gitsigns.lua
@@ -1,10 +1,15 @@
 return {
 	"lewis6991/gitsigns.nvim",
+	event = { "BufReadPre", "BufNewFile" }, -- Load when opening a file
 	config = function()
-		require("gitsigns").setup()
+		local gitsigns = require("gitsigns")
+		gitsigns.setup()
+
 		local map = vim.keymap.set
-		map("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", { desc = "Show Git diff on current line" })
-		map("n", "<leader>gh", ":Gitsigns toggle_current_line_blame<CR>", { desc = "Show Git history on current line" })
-		map("n", "<leader>gu", ":Gitsigns reset_hunk<CR>", { desc = "Undo Git Hunk" })
+		map("n", "<leader>gp", gitsigns.preview_hunk, { desc = "Preview Git hunk" })
+		map("n", "<leader>gh", gitsigns.toggle_current_line_blame, { desc = "Toggle Git blame" })
+		map("n", "<leader>gu", gitsigns.reset_hunk, { desc = "Reset Git hunk" })
+		map("n", "]c", gitsigns.next_hunk, { desc = "Next Git hunk" })
+		map("n", "[c", gitsigns.prev_hunk, { desc = "Previous Git hunk" })
 	end
 }
