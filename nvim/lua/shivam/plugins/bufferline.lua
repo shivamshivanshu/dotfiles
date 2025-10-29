2f58655d5951a71de3e5ff18b87a674f5e2785e1
Shivam Shivanshu <shivamshivanshu15@gmail.com>
2025-10-29 10:13:04 +0530

    Update bufferline.lua

--- nvim/lua/shivam/plugins/bufferline.lua
+++ nvim/lua/shivam/plugins/bufferline.lua
@@ -1,11 +1,11 @@
 return {
-	'akinsho/bufferline.nvim', 
-	version = "*", 
+	'akinsho/bufferline.nvim',
+	version = "*",
 	dependencies = 'nvim-tree/nvim-web-devicons',
-	config = function()
-		require("bufferline").setup()
-		local map = vim.keymap.set
-		map("n", "<Tab>", "<cmd>BufferLineCycleNext<cr>", { desc = "Next Buffer" })
-		map("n", "<S-Tab>", "<cmd>BufferLineCyclePrev<cr>", { desc = "Prev Buffer" })
-	end
+	event = "VeryLazy", -- Defer loading for faster startup
+	keys = {
+		{ "<Tab>", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
+		{ "<S-Tab>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
+	},
+	opts = {},
 }
