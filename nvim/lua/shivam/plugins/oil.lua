a2c697672d4ecea5ca86ce5446506f4039557eb4
Shivam Shivanshu <shivamshivanshu15@gmail.com>
2025-10-29 10:16:50 +0530

    Update oil.lua

--- nvim/lua/shivam/plugins/oil.lua
+++ nvim/lua/shivam/plugins/oil.lua
@@ -1,19 +1,18 @@
 return {
 	'stevearc/oil.nvim',
 	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function()
-		require("oil").setup({
-			view_options = {
-				show_hidden = true,
-			},
-			columns = {
-				"permissions",
-				"size",
-				"mtime",
-			}
-		})
-		local map = vim.keymap.set
-
-		map("n", "-", "<CMD>Oil<CR>", { desc = "Open parent Dir" })
-	end,
+	lazy = false,
+	keys = {
+		{ "-", "<CMD>Oil<CR>", desc = "Open parent directory" },
+	},
+	opts = {
+		view_options = {
+			show_hidden = true,
+		},
+		columns = {
+			"permissions",
+			"size",
+			"mtime",
+		}
+	},
 }
