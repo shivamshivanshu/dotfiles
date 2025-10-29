f40494a4a2ac4ff4f395451191ddfc6bb67e3743
Shivam Shivanshu <shivamshivanshu15@gmail.com>
2025-10-29 10:12:47 +0530

    Update blankline.lua

--- nvim/lua/shivam/plugins/blankline.lua
+++ nvim/lua/shivam/plugins/blankline.lua
@@ -1,8 +1,10 @@
 return {
     "lukas-reineke/indent-blankline.nvim",
+    event = "BufReadPost", -- Load after reading a buffer
     main = "ibl",
     opts = {
         indent = { char = "│" }, -- character for indent guides
         scope = { enabled = true, show_start = true, show_end = true },
     },
 }
+
