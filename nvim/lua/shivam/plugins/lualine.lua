e3d907ca2c8b629f17c53ff71648dc48c5f75890
Shivam Shivanshu <shivamshivanshu15@gmail.com>
2025-10-29 10:16:35 +0530

    Update lualine.lua

--- nvim/lua/shivam/plugins/lualine.lua
+++ nvim/lua/shivam/plugins/lualine.lua
@@ -1,19 +1,18 @@
 return {
 	'nvim-lualine/lualine.nvim',
 	dependencies = { 'nvim-tree/nvim-web-devicons' },
-	config = function()
-		require("lualine").setup({
-			options = {
-				theme = "gruvbox"
-			},
-			sections = {
-				lualine_c = {
-					{
-						'filename',
-						path = 1, -- 0 = filename, 1 = relative path, 2 = absolute path
-					},
+	event = "VeryLazy", -- Defer loading for faster startup
+	opts = {
+		options = {
+			theme = "gruvbox"
+		},
+		sections = {
+			lualine_c = {
+				{
+					'filename',
+					path = 1, -- 0 = filename, 1 = relative path, 2 = absolute path
 				},
 			},
-		})
-	end
+		},
+	},
 }
