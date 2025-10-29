e0143e0cbce4665408c05b3e4bb5b0cbc786c3f7
Shivam Shivanshu <shivamshivanshu15@gmail.com>
2025-10-29 10:16:18 +0530

    Update lsp.lua

--- nvim/lua/shivam/plugins/lsp.lua
+++ nvim/lua/shivam/plugins/lsp.lua
@@ -1,31 +1,36 @@
 return {
 	{
 		"williamboman/mason.nvim",
+		lazy = false, -- Load immediately for LSP setup
 		opts = {},
 		config = function()
 			require("mason").setup()
 		end,
 	},
 	{
-		"williamboman/mason-lspconfig.nvim",
-		opts = {},
-		dependencies = {
-			{ "williamboman/mason.nvim", opts = {} },
-			"neovim/nvim-lspconfig",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		dependencies = { "williamboman/mason.nvim" },
+		opts = {
+			ensure_installed = {
+				-- Formatters
+				"stylua",        -- Lua formatter
+				"black",         -- Python formatter
+				"isort",         -- Python import sorter
+				"clang-format",  -- C/C++ formatter
+				"prettier",      -- JS/TS/JSON/YAML/HTML/CSS formatter
+			},
+			auto_update = true,
 		},
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "clangd", "cmake", "pyright" },
-				automatic_installation = true,
-			})
-		end,
 	},
 	{
 		"neovim/nvim-lspconfig",
 		dependencies = {
 			"hrsh7th/cmp-nvim-lsp",
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
 		},
 		config = function()
+			-- Define on_attach function
 			local on_attach = function(client, bufnr)
 				local map = function(mode, lhs, rhs, desc)
 					vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc })
@@ -37,21 +42,27 @@ return {
 				map("n", "gi", lsp.implementation, "Go to Implementation")
 				map("n", "<leader>rn", lsp.rename, "Rename Symbol")
 				map("n", "<leader>ca", lsp.code_action, "Code Action")
-				map("n", "<leader>f", function() lsp.format({ async = true }) end, "Format Buffer")
 				map("n", "[d", vim.diagnostic.goto_prev, "Prev Diagnostic")
 				map("n", "]d", vim.diagnostic.goto_next, "Next Diagnostic")
 
-				-- Format visual selection
+				-- Format using conform.nvim (fallback to LSP if conform not available)
+				map("n", "<leader>f", function()
+					local ok, conform = pcall(require, "conform")
+					if ok then
+						conform.format({ async = true, lsp_fallback = true })
+					else
+						vim.lsp.buf.format({ async = true })
+					end
+				end, "Format Buffer")
+
+				-- Format visual selection using conform
 				map("x", "<leader>f", function()
-					local s_start = vim.api.nvim_buf_get_mark(0, "<")
-					local s_end   = vim.api.nvim_buf_get_mark(0, ">")
-					vim.lsp.buf.format({
-						async = true,
-						range = {
-							start = { s_start[1] - 1, s_start[2] },
-							["end"] = { s_end[1] - 1, s_end[2] },
-						},
-					})
+					local ok, conform = pcall(require, "conform")
+					if ok then
+						conform.format({ async = true, lsp_fallback = true })
+					else
+						vim.lsp.buf.format({ async = true })
+					end
 				end, "Format Selection")
 
 				-- Enable virtual text inlay hints if supported
@@ -67,49 +78,62 @@ return {
 				capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
 			end
 
-			vim.lsp.config("*", {
-				on_attach = on_attach,
-				capabilities = capabilities,
-			})
+			-- Get lspconfig
+			local lspconfig = require("lspconfig")
 
-			vim.lsp.config("lua_ls", {
-				settings = {
-					Lua = {
-						diagnostics = { globals = { "vim" } },
-						workspace = { checkThirdParty = false },
-					},
-				},
-			})
-
-			vim.lsp.config("cmake", {
-			})
-
-			vim.lsp.config("clangd", {
-				init_options = {
-					clangdFileStatus = true,
-					inlayHints = {
-						parameterNames = true,
-						variableTypes = true,
-						functionReturnTypes = true,
-					},
-				},
-			})
-
-			vim.lsp.config("pyright", {
-				settings = {
-					python = {
-						analysis = {
-							typeCheckingMode = "basic",
-							autoSearchPaths = true,
+			-- Server-specific settings
+			local server_settings = {
+				lua_ls = {
+					settings = {
+						Lua = {
+							diagnostics = { globals = { "vim" } },
+							workspace = { checkThirdParty = false },
 						},
 					},
 				},
-			})
+				clangd = {
+					cmd = {
+						"clangd",
+						"--background-index",
+						"--clang-tidy",
+						"--header-insertion=iwyu",
+						"--completion-style=detailed",
+						"--function-arg-placeholders",
+					},
+					init_options = {
+						clangdFileStatus = true,
+						usePlaceholders = true,
+						completeUnimported = true,
+						semanticHighlighting = true,
+					},
+				},
+				pyright = {
+					settings = {
+						python = {
+							analysis = {
+								typeCheckingMode = "basic",
+								autoSearchPaths = true,
+							},
+						},
+					},
+				},
+				cmake = {},
+			}
 
-			local servers = { "lua_ls", "cmake", "clangd", "pyright" }
-			for _, name in ipairs(servers) do
-				vim.lsp.enable(name)
-			end
+			-- Setup mason-lspconfig with handlers
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "clangd", "cmake", "pyright" },
+				automatic_installation = true,
+				handlers = {
+					-- Default handler for all servers
+					function(server_name)
+						local config = server_settings[server_name] or {}
+						config.on_attach = on_attach
+						config.capabilities = capabilities
+						lspconfig[server_name].setup(config)
+					end,
+				},
+			})
 		end,
 	},
 }
