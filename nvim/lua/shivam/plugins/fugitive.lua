70764a4e9badc0201c5925723f2a66b153185e79
Shivam Shivanshu <shivamshivanshu15@gmail.com>
2025-11-14 20:40:22 +0530

    Update fugitive.lua

--- nvim/lua/shivam/plugins/fugitive.lua
+++ nvim/lua/shivam/plugins/fugitive.lua
@@ -1,5 +1,12 @@
 return {
 	"tpope/vim-fugitive",
-	event = "VeryLazy", -- or "BufReadPre" to load earlier
+	event = "VeryLazy",
+	config = function()
+		local map = vim.keymap.set
+		map("n", "<leader>gs", "<cmd>Git<CR>", { desc = "Git status" })
+		map("n", "<leader>gc", "<cmd>Git commit<CR>", { desc = "Git commit" })
+		map("n", "<leader>gP", "<cmd>Git push<CR>", { desc = "Git push" })
+		map("n", "<leader>gl", "<cmd>Git pull --rebase<CR>", { desc = "Git pull --rebase" })
+		map("n", "<leader>gb", "<cmd>Git blame<CR>", { desc = "Git blame" })
+	end,
 }
-
