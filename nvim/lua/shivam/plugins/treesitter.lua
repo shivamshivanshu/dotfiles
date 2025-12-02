034a917851c1d97b40f1f589dc4bbbfbec1f43db
Shivam Shivanshu <shivamshivanshu15@gmail.com>
2025-12-02 09:42:56 +0530

    Update treesitter.lua

--- nvim/lua/shivam/plugins/treesitter.lua
+++ nvim/lua/shivam/plugins/treesitter.lua
@@ -3,16 +3,18 @@ return {
 	build = ":TSUpdate",
 	event = { "BufReadPost", "BufNewFile" },
 	dependencies = {
-		"nvim-treesitter/nvim-treesitter-textobjects", -- Optional
+		{
+			"nvim-treesitter/nvim-treesitter-textobjects",
+			branch = "main",
+		},
 	},
 	config = function()
 		require("nvim-treesitter.configs").setup({
-			-- Install these languages
 			ensure_installed = {
 				"lua", "python", "bash", "c", "cpp",
-				"json", "markdown", "yaml", "vim", 
+				"json", "markdown", "yaml", "vim",
 			},
-			auto_install = true, -- Automatically install missing parsers
+			auto_install = true,
 
 			highlight = {
 				enable = true,
@@ -42,10 +44,42 @@ return {
 						["if"] = "@function.inner",
 						["ac"] = "@class.outer",
 						["ic"] = "@class.inner",
+						["aa"] = "@parameter.outer",
+						["ia"] = "@parameter.inner",
+					},
+				},
+				move = {
+					enable = true,
+					set_jumps = true,
+					goto_next_start = {
+						["]m"] = "@function.outer",
+						["]]"] = "@class.outer",
+						["]a"] = "@parameter.inner",
+					},
+					goto_next_end = {
+						["]M"] = "@function.outer",
+						["]["] = "@class.outer",
+					},
+					goto_previous_start = {
+						["[m"] = "@function.outer",
+						["[["] = "@class.outer",
+						["[a"] = "@parameter.inner",
+					},
+					goto_previous_end = {
+						["[M"] = "@function.outer",
+						["[]"] = "@class.outer",
+					},
+				},
+				swap = {
+					enable = true,
+					swap_next = {
+						["<leader>a"] = "@parameter.inner",
+					},
+					swap_previous = {
+						["<leader>A"] = "@parameter.inner",
 					},
 				},
 			},
 		})
 	end,
 }
-
