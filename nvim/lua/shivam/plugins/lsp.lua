415a46b477a11d2b2c9ff8550851f131f2322d7e
Shivam Shivanshu <shivamshivanshu15@gmail.com>
2025-12-02 09:42:18 +0530

    Update lsp.lua

--- nvim/lua/shivam/plugins/lsp.lua
+++ nvim/lua/shivam/plugins/lsp.lua
@@ -9,29 +9,33 @@ return {
 	},
 	{
 		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		lazy = false,
 		dependencies = { "williamboman/mason.nvim" },
 		opts = {
 			ensure_installed = {
-				-- Formatters
-				"stylua",        -- Lua formatter
-				"black",         -- Python formatter
-				"isort",         -- Python import sorter
-				"clang-format",  -- C/C++ formatter
-				"prettier",      -- JS/TS/JSON/YAML/HTML/CSS formatter
+				"stylua", -- Lua formatter
+				"clang-format", -- C/C++ formatter
+				"prettier", -- JS/TS/JSON/YAML/HTML/CSS formatter
 			},
 			auto_update = true,
 		},
 	},
 	{
+		"williamboman/mason-lspconfig.nvim",
+		dependencies = { "williamboman/mason.nvim" },
+	},
+	{
 		"neovim/nvim-lspconfig",
+		lazy = false,
 		dependencies = {
 			"hrsh7th/cmp-nvim-lsp",
 			"williamboman/mason.nvim",
 			"williamboman/mason-lspconfig.nvim",
 		},
 		config = function()
-			-- Define on_attach function
 			local on_attach = function(client, bufnr)
+				vim.notify(string.format("LSP attached: %s", client.name), vim.log.levels.INFO)
+
 				local map = function(mode, lhs, rhs, desc)
 					vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc })
 				end
@@ -45,75 +49,75 @@ return {
 				map("n", "[d", vim.diagnostic.goto_prev, "Prev Diagnostic")
 				map("n", "]d", vim.diagnostic.goto_next, "Next Diagnostic")
 
-				-- Enable virtual text inlay hints if supported
 				if client.server_capabilities.inlayHintProvider then
 					vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
 				end
 			end
 
-			-- Enhance capabilities for nvim-cmp
 			local capabilities = vim.lsp.protocol.make_client_capabilities()
 			local ok_cmp, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
 			if ok_cmp then
 				capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
 			end
 
-			-- Get lspconfig
-			local lspconfig = require("lspconfig")
-
-			-- Server-specific settings
-			local server_settings = {
-				lua_ls = {
-					settings = {
-						Lua = {
-							diagnostics = { globals = { "vim" } },
-							workspace = { checkThirdParty = false },
-						},
-					},
-				},
-				clangd = {
-					cmd = {
-						"clangd",
-						"--background-index",
-						"--clang-tidy",
-						"--header-insertion=iwyu",
-						"--completion-style=detailed",
-						"--function-arg-placeholders",
-					},
-					init_options = {
-						clangdFileStatus = true,
-						usePlaceholders = true,
-						completeUnimported = true,
-						semanticHighlighting = true,
-					},
-				},
-				pyright = {
-					settings = {
-						python = {
-							analysis = {
-								typeCheckingMode = "basic",
-								autoSearchPaths = true,
-							},
-						},
-					},
-				},
-				cmake = {},
-			}
-
-			-- Setup mason-lspconfig with handlers
 			require("mason-lspconfig").setup({
 				ensure_installed = { "lua_ls", "clangd", "cmake", "pyright" },
 				automatic_installation = true,
-				handlers = {
-					-- Default handler for all servers
-					function(server_name)
-						local config = server_settings[server_name] or {}
-						config.on_attach = on_attach
-						config.capabilities = capabilities
-						lspconfig[server_name].setup(config)
-					end,
-				},
 			})
+
+			vim.lsp.config.lua_ls = {
+				cmd = { "lua-language-server" },
+				filetypes = { "lua" },
+				root_markers = { ".luarc.json", ".luacheckrc", ".stylua.toml", "stylua.toml", ".git" },
+				on_attach = on_attach,
+				capabilities = capabilities,
+				settings = {
+					Lua = {
+						diagnostics = { globals = { "vim" } },
+						workspace = { checkThirdParty = false },
+					},
+				},
+			}
+
+			vim.lsp.config.clangd = {
+				cmd = {
+					"clangd",
+					"--background-index",
+					"--clang-tidy",
+					"--header-insertion=iwyu",
+					"--completion-style=detailed",
+				},
+				filetypes = { "c", "cpp", "objc", "objcpp", "cuda" },
+				root_markers = { ".clangd", ".clang-tidy", ".clang-format", "compile_commands.json", ".git" },
+				on_attach = on_attach,
+				capabilities = capabilities,
+			}
+
+			vim.lsp.config.pyright = {
+				cmd = { "pyright-langserver", "--stdio" },
+				filetypes = { "python" },
+				root_markers = { "pyrightconfig.json", "pyproject.toml", "setup.py", "requirements.txt", ".git" },
+				on_attach = on_attach,
+				capabilities = capabilities,
+				settings = {
+					python = {
+						analysis = {
+							typeCheckingMode = "basic",
+							autoSearchPaths = true,
+						},
+					},
+				},
+			}
+
+			vim.lsp.config.cmake = {
+				cmd = { "cmake-language-server" },
+				filetypes = { "cmake" },
+				root_markers = { "CMakePresets.json", "CTestConfig.cmake", ".git", "build", "cmake" },
+				on_attach = on_attach,
+				capabilities = capabilities,
+			}
+
+			vim.lsp.enable({ "lua_ls", "clangd", "pyright", "cmake" })
 		end,
 	},
 }
