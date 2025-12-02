8564eefe8991211053175b44f0f0a20c8cb057e4
Shivam Shivanshu <shivamshivanshu15@gmail.com>
2025-12-02 09:41:51 +0530

    Update comment.lua

--- nvim/lua/shivam/plugins/comment.lua
+++ nvim/lua/shivam/plugins/comment.lua
@@ -1,4 +1,9 @@
 return {
 	'numToStr/Comment.nvim',
+	event = { "BufReadPost", "BufNewFile" },
+	keys = {
+		{ "gc", mode = { "n", "v" }, desc = "Comment toggle linewise" },
+		{ "gb", mode = { "n", "v" }, desc = "Comment toggle blockwise" },
+	},
 	opts = {},
 }
